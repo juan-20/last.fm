88c45638b5a86527f1fc65327dc6e0cd89191262
juan-20 <juanalvesandrade@gmail.com>
2023-05-24 13:29:16 -0300

    https request updated

--- src/routes/albuns/[userId]/+page.ts
+++ src/routes/albuns/[userId]/+page.ts
@@ -2,7 +2,7 @@ export const load = ({ fetch, params }) => {
     console.log(params);
 
     const fetchAlbum = async ( id: string) => {
-        const res = await  fetch(`http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${id}&api_key=${import.meta.env.VITE_API_KEY}&format=json`);
+        const res = await  fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${id}&api_key=${import.meta.env.VITE_API_KEY}&format=json`);
         const data = await res.json()
         return data
     }
