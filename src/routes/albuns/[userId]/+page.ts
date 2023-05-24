5d6cd829f38697dec8e4cec331a52a4193f6dd69
juan-20 <juanalvesandrade@gmail.com>
2023-05-24 13:22:39 -0300

    disable SSR

--- src/routes/albuns/[userId]/+page.ts
+++ src/routes/albuns/[userId]/+page.ts
@@ -2,17 +2,21 @@
 // it so that it gets served as a static asset in production
 export const prerender = false;
 
+// disable SSR for this page
+export const ssr = false;
 
 export const load = ({ fetch, params }) => {
-    console.log(params);
+  console.log(params);
 
-    const fetchAlbum = async ( id: string) => {
-        const res = await  fetch(`http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${id}&api_key=${import.meta.env.VITE_API_KEY}&format=json`);
-        const data = await res.json()
-        return data
-    }
+  const fetchAlbum = async (id: string) => {
+    const res = await fetch(
+      `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${id}&api_key=${import.meta.env.VITE_API_KEY}&format=json`
+    );
+    const data = await res.json();
+    return data;
+  };
 
-    return{
-        Album: fetchAlbum(params.userId)
-    }
-  }
+  return {
+    Album: fetchAlbum(params.userId),
+  };
+};
