69dc38ce11a446ae1d93de9b5cb63ef59cfadd3e
juan-20 <juanalvesandrade@gmail.com>
2023-05-26 14:47:53 -0300

    updating https

--- src/routes/albuns/[userId]/[albumCount]/+page.ts
+++ src/routes/albuns/[userId]/[albumCount]/+page.ts
@@ -2,7 +2,7 @@ export const load = ({ fetch, params }) => {
     
     const quantity = Number(params.albumCount) * Number(params.albumCount)
     const fetchAlbum = async ( id: string) => {
-        const res = await  fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=${id}&api_key=${import.meta.env.VITE_API_KEY}&format=json&limit=${quantity}`);
+        const res = await  fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=${id}&api_key=${import.meta.env.VITE_API_KEY}&format=json&limit=${quantity}`);
         const data = await res.json()
         return data
     }
